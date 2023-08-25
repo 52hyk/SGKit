850cda18417075ea7ef561fe86eb10c3002c1e71
hongyongkun <hongyongkun@qq.com>
2023-08-25 18:01:28 +0800

    commit multiple

--- SGDropdown/src/main/java/com/sungrowpower/kit/dropdown/interfaces/SGDropDownCallback.java
+++ SGDropdown/src/main/java/com/sungrowpower/kit/dropdown/interfaces/SGDropDownCallback.java
@@ -12,39 +12,39 @@ public interface SGDropDownCallback {
     /**
      * 弹窗的onCreate方法执行完调用
      */
-    void onCreated(SGBaseView popupView);
+    void onCreated(SGBaseView sgBaseView);
 
     /**
      * 在show之前执行，由于onCreated只执行一次，如果想多次更新数据可以在该方法中
      */
-    void beforeShow(SGBaseView popupView);
+    void beforeShow(SGBaseView sgBaseView);
 
     /**
      * 完全显示的时候执行
      */
-    void onShow(SGBaseView popupView);
+    void onShow(SGBaseView sgBaseView);
 
     /**
      * 完全消失的时候执行
      */
-    void onDismiss(SGBaseView popupView);
+    void onDismiss(SGBaseView sgBaseView);
 
     /**
      * 准备消失的时候执行
      */
-    void beforeDismiss(SGBaseView popupView);
+    void beforeDismiss(SGBaseView sgBaseView);
 
     /**
      * 暴漏返回按键的处理，如果返回true，Dropdown不会处理；如果返回false，Dropdown会处理，
      * @return
      */
-    boolean onBackPressed(SGBaseView popupView);
+    boolean onBackPressed(SGBaseView sgBaseView);
 
     /**
      * 当软键盘高度改变时调用，比如打开和关闭软键盘
      * @param height
      * @return
      */
-    void onKeyBoardStateChanged(SGBaseView popupView, int height);
+    void onKeyBoardStateChanged(SGBaseView sgBaseView, int height);
 
 }
